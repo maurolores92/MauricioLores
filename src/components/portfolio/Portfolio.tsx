44f3cf76672ddd889969563e3a9c24c727677024
Mauricio <maurolores1992@gmail.com>
2024-04-04 17:09:37 -0300

    agregando out

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -39,13 +39,14 @@ const Portfolio = () => {
         height: "100%", 
         display: "flex", 
         flexDirection: "column", 
-        alignItems: 'center'
+        alignItems: 'center',
+        width: '100%'
         }}>
         <Box sx={{ paddingTop: '4rem', maxWidth: { md: '1000px', lg: '1200px' }, width: '100%' }}>
           <Typography color='white' fontWeight='900' sx={{ fontSize: { xs: '28px', lg: '48px' }, margin: '2rem auto', textAlign:'center' }}>Portfolio</Typography>
           <Typography color='white' fontWeight='400' sx={{ maxWidth: '800px', margin: '1rem auto', textAlign:'center' }}>Aquí hay algunos de mis proyectos recientes que muestran mis habilidades y experiencia en desarrollo web. Cada proyecto representa un desafío único que pude superar con creatividad y atención al detalle. ¡Echa un vistazo y descubre lo que puedo hacer por ti!  </Typography>
         </Box>
-        <Box sx={{maxWidth:'1400px', margin:'0 auto', textAlign:'center'}}>
+        <Box sx={{maxWidth:'1400px', margin:'0 auto'}}>
         <Slider {...settings}>
           {portfolioData.map((servicio, index) => (
               <Card key={servicio.id} sx={{ margin:'1rem auto', borderRadius:'12px', width:'338px', height:'463px'  }} className="container">
