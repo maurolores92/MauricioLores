d50aae5e05acd245b17b775220546556724459e7
Mauricio <maurolores1992@gmail.com>
2024-04-04 17:05:44 -0300

    agregando carousel

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,23 +1,61 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import portfolioData from './portfolio.json';
-import { Typography,  Box, Button} from '@mui/material';
+import { Card, CardContent, Typography,  Box, Button, Divider} from '@mui/material';
 
 const Portfolio = () => {
+  const [slidesToShow, setSlidesToShow] = useState(1);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToScroll: slidesToShow, // Ajustado a slidesToShow
+    slidesToShow: slidesToShow
+  };
 
-
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 1224) {
+        setSlidesToShow(3); 
+      } else if (window.innerWidth >= 730) {
+        setSlidesToShow(2);
+      } else {
+        setSlidesToShow(1);
+      }
+    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize); 
+    };
+  }, []);
+  
     return (
-      <Box sx={{ backgroundColor: '#2D3540', height: "100%", display: "flex", flexDirection: "column", alignItems: 'center', }}>
+      <>      
+      <Box sx={{ 
+        backgroundColor: '#2D3540', 
+        height: "100%", 
+        display: "flex", 
+        flexDirection: "column", 
+        alignItems: 'center'
+        }}>
         <Box sx={{ paddingTop: '4rem', maxWidth: { md: '1000px', lg: '1200px' }, width: '100%' }}>
           <Typography color='white' fontWeight='900' sx={{ fontSize: { xs: '28px', lg: '48px' }, margin: '2rem auto', textAlign:'center' }}>Portfolio</Typography>
           <Typography color='white' fontWeight='400' sx={{ maxWidth: '800px', margin: '1rem auto', textAlign:'center' }}>Aquí hay algunos de mis proyectos recientes que muestran mis habilidades y experiencia en desarrollo web. Cada proyecto representa un desafío único que pude superar con creatividad y atención al detalle. ¡Echa un vistazo y descubre lo que puedo hacer por ti!  </Typography>
         </Box>
-        <Box sx={{ display: 'flex', justifyContent: 'space-evenly', margin: '4rem 0 4rem 0', flexWrap: 'wrap', }}>
+        <Box sx={{maxWidth:'1400px', margin:'0 auto', textAlign:'center'}}>
+        <Slider {...settings}>
           {portfolioData.map((servicio, index) => (
-            <Box key={servicio.id} style={{ marginRight: index < portfolioData.length - 1 ? '16px' : '0', marginTop:'1rem' }} className="container">
-              <img src={servicio.imagen} alt={servicio.title} style={{ width: '330px', height: '220px', borderRadius: '12px', margin: '1rem auto' }} /> 
+              <Card key={servicio.id} sx={{ margin:'1rem auto', borderRadius:'12px', width:'338px', height:'463px'  }} className="container">
+                <Box sx={{ position: 'relative' }}>
+              <img src={servicio.imagen} alt={servicio.title} style={{ width: '330px', height: '220px', borderRadius: '12px', margin: '.5rem auto' }} /> 
+              </Box>
+              <CardContent>
               <Box sx={{height:'240px'}}>
               <Typography variant='h4' color="#6B7280">{servicio.title}</Typography>
-              <Typography variant='body1' color="#6B7280" sx={{margin:'1rem'}}>{servicio.description}</Typography>
+              <Typography variant='body1' color="#6B7280" >{servicio.description}</Typography>
               <Box style={{ display: "flex", flexWrap: "wrap", marginTop: "8px", justifyContent: 'center' }}>
                 {Object.values(servicio.labels).map((label, index) => (
                 <Typography
@@ -25,7 +63,7 @@ const Portfolio = () => {
                   color="#FFFFFF"
                   fontWeight="700"
                   fontSize="16px"
-                  style={{ margin: "8px", backgroundColor: '#6B7280', padding: '4px 12px 4px 12px', borderRadius: '6px' }}>
+                  style={{ margin: "6px", backgroundColor: '#6B7280', padding: '4px 10px 4px 10px', borderRadius: '6px' }}>
                   {label}
                 </Typography>))}
               </Box>
@@ -35,10 +73,14 @@ const Portfolio = () => {
                   </a>
                 </Box>
               </Box>
-            </Box>
+              </CardContent>
+              </Card>
           ))}
+          </Slider>
         </Box>
       </Box>
+      </>
+
     );
   };
   
