2ccf95fdbf709c15dd53f95580c594fbeeb1fcf7
Mauricio <maurolores1992@gmail.com>
2024-04-17 20:35:35 -0300

    mofidicando

--- src/components/certifications/Certifications.tsx
+++ src/components/certifications/Certifications.tsx
@@ -57,10 +57,10 @@ const Certifications = () => {
       backgroundColor: '#2D3540', 
       display: 'flex', 
       flexDirection: 'column', 
-      alignItems: 'center' 
+      alignItems: 'center' ,
       }}>
       <Box sx={{ paddingTop: {xs:'0', lg:'4rem'}, maxWidth: { md: '1000px', lg: '1200px' }, width: '100%' }}>
-        <Typography color='white' fontWeight='900' sx={{ fontSize: { xs: '28px', lg: '48px' }, margin:  {xs:'0 auto', lg:'2rem auto'}, textAlign: 'center' }}>Certificaciones</Typography>
+        <Typography color='white' fontWeight='900' sx={{ fontSize: { xs: '28px', lg: '48px' }, margin:  {xs:'2rem auto', lg:'2rem auto'}, textAlign: 'center' }}>Certificaciones</Typography>
         <Typography color='white' fontWeight='400' sx={{ maxWidth: '800px', margin: '1rem auto', textAlign:'center' }}>Mi compromiso con la excelencia y el aprendizaje continuo se refleja en las certificaciones que he obtenido en el campo de la programacion. Estas certificaciones son el resultado de mi dedicación a mejorar mis habilidades y mantenerme al día con las últimas tecnologías y mejores prácticas. </Typography>
       </Box>
       <Box sx={{width: {xs: '390px', md: '800px', lg:'1400px'}, margin:'2rem auto'}}>
