43a675a08920c7db7a54bdf13e23ba14398c2e08
Mauricio <maurolores1992@gmail.com>
2024-04-26 11:49:09 -0300

    modif images

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 
 import HomeMauro from "../src/components/home/home";
-import AboutMe from "../src/components/aboutMe/AboutMe";
 import Portfolio from "../src/components/portfolio/Portfolio";
 import Technologies from "../src/components/technologies/Technologies";
 import Footer from "../src/components/footer/footer";
--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -41,7 +41,7 @@ function HomeMauro() {
       }}
     >
       <Box sx={{ textAlign: 'center', alignItems:'center', margin:'2rem' }}>
-            <img src="images/MauricioLores1.svg" alt="" style={{ width: '300px', height: 'auto', borderRadius: '12px'}} />
+            <img src="images/MauricioLores1.svg " alt="" style={{ width: '300px', height: 'auto', borderRadius: '12px'}} />
         </Box>
         <Box sx={{ width:'maxContent', marginTop: {xs: '0', lg:'6rem'}}}>
         <Typography fontWeight="bold" sx={{textAlign:'center', fontSize: {xs:'30px', md:'48px  '},}}>
