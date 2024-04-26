a98a1f8e8b49493f3297d3458ff01631a1be7c75
Mauricio <maurolores1992@gmail.com>
2024-04-26 10:05:21 -0300

    agregando speed insights

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,16 +5,19 @@ import Portfolio from "../src/components/portfolio/Portfolio";
 import Technologies from "../src/components/technologies/Technologies";
 import Footer from "../src/components/footer/footer";
 import Certifications from "../src/components/certifications/Certifications";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function Home() {
 
   return (
       <>
+
       <HomeMauro/>
       <Technologies/>
       <Portfolio/>
       <Certifications/>
       <Footer/>
+      <SpeedInsights/>
       </>
   );
 }
