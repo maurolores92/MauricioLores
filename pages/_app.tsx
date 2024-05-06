6659c3a3656e3eea2f55ff3bc8f83aecad59e238
Mauricio <maurolores1992@gmail.com>
2024-05-05 22:10:08 -0300

    modif

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -49,6 +49,7 @@ export default function MyApp(props: MyAppProps) {
         <meta property="og:site_name" content="Desarrollador Frontend | Diseño y Programación Web | Mauricio Lores" />
         <meta property="og:type" content="website" />
         <meta property="og:locale" content="es_ES" />
+        <link rel="canonical" href="https://www.mauriciolores.com.ar/" />
         <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png" />
